6b0ba1a20d8d632f1337c37d2bcdae6bbf476bba
dignityloss <ultimatefnafgamer@gmail.com>
2026-01-18 20:43:58 +0200

    rolling cage fixes

--- scenes/towers/rolling cage/rolling_cage.gd
+++ scenes/towers/rolling cage/rolling_cage.gd
@@ -1,12 +1,18 @@
 class_name RollingCage
 extends BaseTower
 
+const GRAVITY = 10
+
 @export var speed: float = 20
 
 func _physics_process(delta) -> void:
 	super(delta)
 	if is_on_wall():
 		speed = -speed
+	if !is_on_floor():
+		velocity.y += GRAVITY
+	else:
+		velocity.y = 0
 	velocity.x = speed
 	move_and_slide()
 
